03f0c5b8858205e85bca5601b0e1765a7cd0e7f9
VladislavSolodjankin <Vladislav_Solodjankin@epam.com>
2015-12-01 09:19:43 +0400

    ReactTest 3

--- src/js/components/DescriptionPage.jsx
+++ src/js/components/DescriptionPage.jsx
@@ -3,6 +3,7 @@ var DescriptionPage = React.createClass({
     return {
       description: [
         {name:'GIGABYTE-GA-78LMT-S2', img:'img/CPU/Intel/Intel%20Celeron%20G1620%20OEM.jpg', description:'Технические характеристики Процессор Intel Celeron G1820 OEM Intel Celeron четвертого поколения, тактовая частота 2700 МГц, тепловыделение 53 Ватт, сокет LGA 1150, двухканальный режим памяти.', minImg1: 'img/CPU/Intel/Intel%20Pentium%20G2020%20OEM.jpg', minImg2:'img/CPU/Intel/Intel%20Pentium%20G3220%20BOX.jpg', minImg3: 'img/CPU/Intel/Intel%20Pentium%20G3420%20OEM.jpg', minImg4: 'img/CPU/Intel/Intel%20Pentium%20G3440%20BOX.jpg', minImg5: 'img/CPU/Intel/Intel%20Pentium%20G3440%20BOX.jpg'},
+          {name:'GIGABYTE-GA-78LMT-S2', img:'img/CPU/Intel/Intel%20Celeron%20G1620%20OEM.jpg', description:'Технические характеристики Процессор Intel Celeron G1820 OEM Intel Celeron четвертого поколения, тактовая частота 2700 МГц, тепловыделение 53 Ватт, сокет LGA 1150, двухканальный режим памяти.', minImg1: 'img/CPU/Intel/Intel%20Pentium%20G2020%20OEM.jpg', minImg2:'img/CPU/Intel/Intel%20Pentium%20G3220%20BOX.jpg', minImg3: 'img/CPU/Intel/Intel%20Pentium%20G3420%20OEM.jpg', minImg4: 'img/CPU/Intel/Intel%20Pentium%20G3440%20BOX.jpg', minImg5: 'img/CPU/Intel/Intel%20Pentium%20G3440%20BOX.jpg'},
 
       ],
     }
@@ -12,8 +13,6 @@ render: function () {
     return (
         <div className="product-section__product-description">
           <DescriptionList description={this.props.description}/>
-
-
         </div>
     );
   }
