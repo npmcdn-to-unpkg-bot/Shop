ccc8e5fbd2debbb432a0937e47a1091ef4488518
VladislavSolodjankin <Vladislav_Solodjankin@epam.com>
2015-12-03 17:00:56 +0400

    Single-page.V.7

--- src/js/Search.js
+++ src/js/Search.js
@@ -4,7 +4,7 @@ var objAll= [{
          img:'elementspagelink__img--img1',
          price:'$ 555',
          link:'#GIGABYTE-GA-78LMT-S2',
-         description: 'Технические характеристики Процессор Intel Celeron G1820 OEM Intel Celeron четвертого поколения, тактовая частота 2700 МГц, тепловыделение 53 Ватт, сокет LGA 1150, двухканальный режим памяти.',},
+         description: 'Технические характеристики Процессор Intel Celeron G1820 OEM Intel Celeron четвертого поколения, тактовая частота 2700 МГц, тепловыделение 53 Ватт, сокет LGA 1150, двухканальный режим памяти.'},
         {name:'GIGABYTE-GA-78LMT-S3',
          img:'elementspagelink__img--img2',
          price:'$ 655',
@@ -18,57 +18,62 @@ var objAll= [{
 		{name:'GIGABYTE-GA-78LMT-S5',
          img:'elementspagelink__img--img4',
          price:'$ 900', link:'#GIGABYTE-GA-78LMT-S2',
-         description: 'Технические характеристики Процессор Intel Celeron G1820 OEM Intel Celeron четвертого поколения, тактовая частота 2700 МГц, тепловыделение 53 Ватт, сокет LGA 1150, двухканальный режим памяти.',}  
+         description: 'Технические характеристики Процессор Intel Celeron G1820 OEM Intel Celeron четвертого поколения, тактовая частота 2700 МГц, тепловыделение 53 Ватт, сокет LGA 1150, двухканальный режим памяти.'}  
     ],
     CPU: [
         {name:'GIGABYTE-GA-78LMT-S2',
          img:'elementspagelink__img--img1',
          price:'$ 555',
          link:'#GIGABYTE-GA-78LMT-S2',
-         description: 'Технические характеристики Процессор Intel Celeron G1820 OEM Intel Celeron четвертого поколения, тактовая частота 2700 МГц, тепловыделение 53 Ватт, сокет LGA 1150, двухканальный режим памяти.',},
+         description: 'Технические характеристики Процессор Intel Celeron G1820 OEM Intel Celeron четвертого поколения, тактовая частота 2700 МГц, тепловыделение 53 Ватт, сокет LGA 1150, двухканальный режим памяти.'},
     ],
     Ram:[
         {name:'GIGABYTE-GA-78LMT-S2',
          img:'elementspagelink__img--img1',
          price:'$ 555',
          link:'#GIGABYTE-GA-78LMT-S2',
-         description: 'Технические характеристики Процессор Intel Celeron G1820 OEM Intel Celeron четвертого поколения, тактовая частота 2700 МГц, тепловыделение 53 Ватт, сокет LGA 1150, двухканальный режим памяти.',},
+         description: 'Технические характеристики Процессор Intel Celeron G1820 OEM Intel Celeron четвертого поколения, тактовая частота 2700 МГц, тепловыделение 53 Ватт, сокет LGA 1150, двухканальный режим памяти.'},
     ],
     VideoAdapter: [
         {name:'GIGABYTE-GA-78LMT-S2',
          img:'elementspagelink__img--img1',
          price:'$ 555',
          link:'#GIGABYTE-GA-78LMT-S2',
-         description: 'Технические характеристики Процессор Intel Celeron G1820 OEM Intel Celeron четвертого поколения, тактовая частота 2700 МГц, тепловыделение 53 Ватт, сокет LGA 1150, двухканальный режим памяти.',},
+         description: 'Технические характеристики Процессор Intel Celeron G1820 OEM Intel Celeron четвертого поколения, тактовая частота 2700 МГц, тепловыделение 53 Ватт, сокет LGA 1150, двухканальный режим памяти.'},
     ],
     HDD: [
         {name:'GIGABYTE-GA-78LMT-S2',
          img:'elementspagelink__img--img1',
          price:'$ 555',
          link:'#GIGABYTE-GA-78LMT-S2',
-         description: 'Технические характеристики Процессор Intel Celeron G1820 OEM Intel Celeron четвертого поколения, тактовая частота 2700 МГц, тепловыделение 53 Ватт, сокет LGA 1150, двухканальный режим памяти.',},
+         description: 'Технические характеристики Процессор Intel Celeron G1820 OEM Intel Celeron четвертого поколения, тактовая частота 2700 МГц, тепловыделение 53 Ватт, сокет LGA 1150, двухканальный режим памяти.'},
     ],
     PowerSource: [
         {name:'GIGABYTE-GA-78LMT-S2',
          img:'elementspagelink__img--img1',
          price:'$ 555',
          link:'#GIGABYTE-GA-78LMT-S2',
-         description: 'Технические характеристики Процессор Intel Celeron G1820 OEM Intel Celeron четвертого поколения, тактовая частота 2700 МГц, тепловыделение 53 Ватт, сокет LGA 1150, двухканальный режим памяти.',},
+         description: 'Технические характеристики Процессор Intel Celeron G1820 OEM Intel Celeron четвертого поколения, тактовая частота 2700 МГц, тепловыделение 53 Ватт, сокет LGA 1150, двухканальный режим памяти.'},
     ],
     Cooling: [
         {name:'GIGABYTE-GA-78LMT-S2',
          img:'elementspagelink__img--img1',
          price:'$ 555',
          link:'#GIGABYTE-GA-78LMT-S2',
-         description: 'Технические характеристики Процессор Intel Celeron G1820 OEM Intel Celeron четвертого поколения, тактовая частота 2700 МГц, тепловыделение 53 Ватт, сокет LGA 1150, двухканальный режим памяти.',},
+         description: 'Технические характеристики Процессор Intel Celeron G1820 OEM Intel Celeron четвертого поколения, тактовая частота 2700 МГц, тепловыделение 53 Ватт, сокет LGA 1150, двухканальный режим памяти.'},
     ]
 }];
 
-
+$(document).ready(function(){
+    $(document).on('keydown', '.all__header__elements--search',  function(){
     var i = objAll.length;
-console.log(i);
     var inputSearch = $('.all__header__elements--search').val();
-    for (var k=0; k<2; k++){
-         console.log(objAll[0].Motherboard[k].name);
-    };
 
+    for (var k=0; k<1; k++){
+
+
+         console.log(objAll[0].Motherboard[k].name.toLowerCase().indexOf(inputSearch.toLowerCase()));
+   
+    };
+    });
+});
