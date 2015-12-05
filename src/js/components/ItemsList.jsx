c9cbca028ad8698e2aba6dd6a7df880c9ca5d1a7
OlegBaikov <Oleg_Baikov@epam.com>
2015-12-05 15:29:37 +0400

    v.1 5.12.2015

--- src/js/components/ItemsList.jsx
+++ src/js/components/ItemsList.jsx
@@ -3,15 +3,15 @@ import { render } from 'react-dom'
 import { Router, Route, Link } from 'react-router'
 var ItemsList = React.createClass ({
 	render: function () {
-	  var categories = this.props.categories.map(function(category, key) {
+	  var items = this.props.items.map(function(item, key) {
 		return (
 		  <li className="newv__tab-content__layoutitems" key = {key}>
 		    <Link className="newv__Categories newv__pages__items" query={{ foo: 'bar' }}  to="/Motherboard/GIGABYTE-GA-78LMT-S2">
 		      <figure className="newv__elementspagefigure">
-		        <div className={"newv__elementspagelink__img " + category.img}>
+		        <div className={"newv__elementspagelink__img " + item.img}>
 		        </div>
 		        <figcaption className="newv__elementspage__figcaption">
-		          {category.name}<br />{category.price}
+		          {item.name}<br />{item.price}
 		        </figcaption>
 		      </figure>
 		    </Link>
@@ -22,7 +22,7 @@ var ItemsList = React.createClass ({
 		
 	  return (
 		<ul className="newv__product-section__tab-content newv__tab-content">
-		  {categories}
+		  {items}
         </ul>			
 	  );
 	}
