5e9db5155c3f7f28093197533d57d7bfb68d9523
VladislavSolodjankin <Vladislav_Solodjankin@epam.com>
2015-12-10 17:04:02 +0400

    Slider1

--- src/js/components/CartTemplate.jsx
+++ src/js/components/CartTemplate.jsx
@@ -8,7 +8,60 @@ var CartTemplate = React.createClass ({
 			)				
 		});*/
 		return (
-			<div>HelloWorld!</div>
+			<div className="cartpage__section" id="cartpage__item">
+			<div className="cartpage__cart">
+				<div className="itemscart">
+					<div className="itemscart__cellview">
+						<div className="itemscart__boximg">
+							<a className="itemscart__imglink itemscart__imglink--style" href="productdescription.html">
+								<div className="imgitemtable img1">
+								</div>
+							</a>
+						</div>
+						<div className="descriptionitem">
+							<div className="linkproperties__cart linkproperties__cart--style">
+								<a className="elementspagelink elementspagelink--style" href="productdescription.html">
+									AMD A8-7600 OEM
+								</a>
+							</div>
+							<div className="descriptionitem__count">
+								<div className="cartquant">
+									Quantity
+								</div>
+								<div className="cartinput">
+									<input type="number" min="1" max="200" value="1" name="quality" />
+								</div>
+								<div className="cartpriceview">
+									$ 500
+								</div>
+								<div className="itemsremove">
+									<input type="button" value="Remove" name="remove" />
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="cartpage__carttotal">
+				<div className="totalpricetable">
+					Total: $ 3000
+				</div>
+				<div className="paytable">
+					<a className="totalbar__link" href="AddressPage.html">
+						<div className="tocheckout">
+							Proceed to Checkout
+						</div>
+					</a>
+				</div>
+				<div className="paytable">
+					<a className="totalbar__link" href="index.html">
+						<div className="tocheckout">
+							Continue Shopping
+						</div>
+					</a>
+				</div>
+			</div>
+			</div>
 		);
 	}
 });
