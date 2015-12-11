8abd54b626e38a3097932efa113ea0ac0bf5ae12
OlegBaikov <Oleg_Baikov@epam.com>
2015-12-11 12:40:34 +0400

    v.3 11.12.2015

--- src/js/components/addres.jsx
+++ src/js/components/addres.jsx
@@ -2,7 +2,7 @@ import {Header} from 'Header.jsx'
 import {Footer} from 'Footer.jsx'
 import { Router, Route, IndexRoute, Link, IndexLink } from 'react-router'
 	
-var Addres = React.class({
+var Addres = React.createClass({
 	render: function () {
 		return (
 			<div className="page">
