cfde9569d06976319c600c1bcc5c8190e830408f
VladislavSolodjankin <Vladislav_Solodjankin@epam.com>
2015-12-11 13:04:28 +0400

    React.V.2.5

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -7,7 +7,10 @@ import {Description} from 'DescriptionPage.jsx'
 import {Cart} from 'cart.jsx'
 import {Pay} from 'Pay.jsx'
 import {Addres} from 'addres.jsx'
+<<<<<<< HEAD
 import {Finish} from 'Finish.jsx'
+=======
+>>>>>>> 8abd54b626e38a3097932efa113ea0ac0bf5ae12
 
 var app = React.createClass({
 	render: function () {
@@ -17,10 +20,14 @@ var app = React.createClass({
 			<Route path="/Categories/:category" component={Category} />
            	<Route path="/Categories/:category/:id" component={Description} />		
 			<Route path="/Cart" component={Cart} />
+<<<<<<< HEAD
 			<Route path="/Cart/Addres" component={Addres} />
 			<Route path="/Cart/Addres/Pay" component={Pay} />
 			<Route path="/Cart/Addres/Pay/Finish" component={Finish} />
 
+=======
+			<Route path="/Cart/Addres" component={Addres} />						
+>>>>>>> 8abd54b626e38a3097932efa113ea0ac0bf5ae12
     	</Router>
     );
   }
