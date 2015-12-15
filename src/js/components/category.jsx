d8be330759889f5d3bfbd8a8d662442d807aebe8
OlegBaikov <Oleg_Baikov@epam.com>
2015-12-15 18:56:57 +0400

    v.3 15.12.2015

--- src/js/components/category.jsx
+++ src/js/components/category.jsx
@@ -4,28 +4,41 @@ import {Footer} from 'Footer.jsx'
 import {Router, Route, IndexRoute, Link, IndexLink} from 'react-router'
 import {Navigation} from 'Navigation.jsx'
 import {ItemsList} from 'ItemsList.jsx'
+import {Goods} from 'GoodsStore.jsx'
+ 	
 var Category = React.createClass({
   	
 	getInitialState: function () {
-		return {			
+		return {
+			items: [],
         	category: this.props.params.category,
         };
   	},
     
-	getItems: function(category) {
+	/*getItems: function(category) {
 		var arrCategories = ['Motherboard', 'CPU', 'RAM', 'VideoAdapter', 'HDD', 'PowerSource', 'Cooling'];
 		for (var i = 0; i <arrCategories.length; i++) {
 			if (category == arrCategories[i]) {				
 				return this.props[arrCategories[i]];
       		}					
 		}
-    },
+    },*/
+	
 	componentWillReceiveProps: function (newProps) {
 		this.setState ({
 			category: newProps.params.category
 		})	
 	},
-	getDefaultProps: function () {
+	
+	/*componentDidMount: function(){
+        Dispatcher.on('update-goods', this.update);
+        Dispatcher.emit('get-goods');
+    },
+	
+    componentWillUnMount: function(){
+        Dispatcher.off('update-goods', this.update);
+    },*/
+/*	getDefaultProps: function () {
 		return {
 			Motherboard: [
 				{category: 'Motherboard', name:'GIGABYTE-GA-78LMT-S2', img:'unit-value__img-moth1', price:'$ 555', link:'GIGABYTE-GA-78LMT-S2'}, 
@@ -73,9 +86,24 @@ var Category = React.createClass({
 			],
 
 		}
-	},	
+	},*/	
 
 	render: function () {
+		var items = this.state.items.map(function(items, key) {
+			return (
+			  <li className="goods__unit" key = {key}>
+				<Link className="unit-value" to={'/Categories/'+items.category+'/'+items.link} >
+				  <figure className="unit-value__figure">
+					<div className={"unit-value__img " + items.img}>
+					</div>
+					<figcaption className="unit-value__figcaption">
+					  {items.name}<br />{items.price}
+					</figcaption>
+				  </figure>
+				</Link>
+			  </li>
+			);
+	  	});
 		return (
 			<div className="page">
 			
@@ -89,14 +117,15 @@ var Category = React.createClass({
 
 					<article className="goods goods--style">      
 						<div className="goods__view">
-							<Link className="viewmode" to={this.state.category} >
+							<Link className="viewmode" to={'/'} >
 								List View
 							</Link>
-						</div>          
-						<ItemsList items={this.getItems(this.state.category)} />					
+						</div>
+						<ul className="goods__folder">
+		  					{items}
+        				</ul>									
 					</article>
-                </div>
-											
+                </div>											
                 <div className="page__footer">
 				    <Footer />
                 </div>
